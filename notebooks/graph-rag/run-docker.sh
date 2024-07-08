9a797b4402cf93c714d869300f1a054b75d81017
Alex Morales <44649358+msalemor@users.noreply.github.com>
2024-07-08 15:42:34 -0400

    Add files via upload

---
+++ notebooks/graph-rag/run-docker.sh
@@ -0,0 +1,10 @@
+docker run \
+    -p 7474:7474 -p 7687:7687 \
+    -v $PWD/data:/data -v $PWD/plugins:/plugins \
+    --name neo4j-apoc \
+    -e NEO4J_apoc_export_file_enabled=true \
+    -e NEO4J_apoc_import_file_enabled=true \
+    -e NEO4J_apoc_import_file_use__neo4j__config=true \
+    -e NEO4JLABS_PLUGINS=\[\"apoc\"\] \
+    -e NEO4J_dbms_security_procedures_unrestricted=apoc.\\\* \
+    --rm neo4j
