6807dc5e3193b5b7b6978a9816d0354e38332f43
Alex Morales <alemor@microsoft.com>
2025-03-28 20:13:29 -0400

    Added more query examples

--- autogen-multiagent/agent_kql_generator.py
+++ autogen-multiagent/agent_kql_generator.py
@@ -14,7 +14,7 @@ async def mock_get_schema(name: str) -> dict:
     await asyncio.sleep(.1)
     if name == "logs":
         return {
-            "cluster": "loggingevents",
+            "cluster": "loggingevents.contoso.com",
             "database": "logs",
             "table": "events",
             "fields": "id (string),userid (string), ts (timestamp), event (string), systemid (string), type (string)",
@@ -22,7 +22,7 @@ async def mock_get_schema(name: str) -> dict:
         }
     if name == "users":
         return {
-            "cluster": "loggingevents",
+            "cluster": "loggingevents.contoso.com",
             "database": "logs",
             "table": "users",
             "fields": "userid (string), name (string)",
@@ -30,7 +30,7 @@ async def mock_get_schema(name: str) -> dict:
         }
     if name == "system":
         return {
-            "cluster": "master",
+            "cluster": "master.contoso.com",
             "database": "services",
             "table": "systems",
             "fields": "systemid (string), name (string), ha (boolean)",
@@ -56,7 +56,7 @@ gen_kql_query = AssistantAgent(
     system_message="""You are an AI that can generate a KQL queries based on the schema provided. 
 If the cluster and database names are provided, use them in the generated query. 
 Example with cluster and database name: 
-cluster('name').database('table').events | where ts>ago(24h)
+cluster('name.domain.com').database('table').events | where ts>ago(24h)
 
 Once generate write 'TERMINATE'.\n""",
 )
@@ -71,7 +71,11 @@ team = RoundRobinGroupChat(
 
 async def main():
     await Console(team.run_stream(
-        task="Write a query to find all events by user name and system name in the last 24 hours?",))
+        task="Find all the infra events in the last 1 hour",))
+    await Console(team.run_stream(
+        task="Find all the codes events in the last 1 hour. Show the user name.",))
+    await Console(team.run_stream(
+        task="Write a query to find all events by user name and system name in the last 24 hours",))
 
 if __name__ == "__main__":
     asyncio.run(main())
